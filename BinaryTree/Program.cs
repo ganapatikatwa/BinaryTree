81ccefe22ce318d6cb71430d78b811ced04312ad
[ganapat] <ganapatisakatwa@gmail.com>
2022-09-07 17:53:22 +0530

    [Ganapati]Creating tree

---
+++ BinaryTree/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BinaryTree
+{
+    public class Program
+    {
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("Welcome to Binary Search Tree problem");
+            BinarySearchTree<int> BinaryTree = new BinarySearchTree<int>();
+
+            BinaryTree.AddNode(56);
+            BinaryTree.AddNode(30);
+            BinaryTree.AddNode(70);
+            BinaryTree.AddNode(22);
+        }
+    }
+}
